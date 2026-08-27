7a6419cb42531fff2f4eb41b83e90c9e6e8a75f7
JAVIERUCHIHA <javiermadarax500@gmail.com>
2026-08-27 03:37:05 -0400

    Update main.js

--- main.js
+++ main.js
@@ -101,6 +101,7 @@ class JurisprudenciaScraper {
         // Inicializar navegador optimizado para Render
         this.browser = await puppeteer.launch({
             headless: true,
+            executablePath: '/usr/bin/chromium',
             args: [
                 '--no-sandbox',
                 '--disable-setuid-sandbox',
